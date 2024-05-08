3ff469761affeec177d07a52d22382631b526d6c
SamuelPhilMS <kittylivvy00@gmail.com>
2024-05-08 10:34:27 -0500

    attempting to switch from the static map to a dynamic one

--- src/main/resources/static/map/mapScript.js
+++ src/main/resources/static/map/mapScript.js
@@ -1,5 +1,39 @@
+//api documentation examples
+/*let map;
 
-        google.charts.load("current", {
+async function initMap(google) {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map_div"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();*/
+
+// youtuber example
+/*(function(window, google){
+
+FOR mapScript !! -> <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+//map options
+let options = {
+center: {
+lat : 38.6270,
+lng : -90.1994 },
+zoom: 10
+}
+element = document.getElementById('map_div');
+//map
+map = new google.map.Maps(element, options)
+
+}(window, google))*/
+
+//the og static map
+  /*
+      USE IN mapScript.js !!! ->  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+      google.charts.load("current", {
           "packages":["map"],
           "mapsApiKey": "AIzaSyDgfwP9_Iur5vGE5suxMVNq3qwiONfMq2I"
         });
@@ -25,4 +59,4 @@
                showInfoWindow: true
              });
 
-           }
+           }*/
